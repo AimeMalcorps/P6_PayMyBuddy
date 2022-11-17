429c903e3f10a6bcc1c46f5e5bef04ba5f212cc0
Aimé Malcorps <aime.malcorps@neuf.fr>
2022-11-17 15:07:47 +0100

    Encrypt Database Access

--- paymybuddy/src/main/java/com/paymybuddy/PayMyBuddyApplication.java
+++ paymybuddy/src/main/java/com/paymybuddy/PayMyBuddyApplication.java
@@ -3,7 +3,10 @@ package com.paymybuddy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+
 @SpringBootApplication
+@EnableEncryptableProperties
 public class PayMyBuddyApplication {
 
 	public static void main(String[] args) {
